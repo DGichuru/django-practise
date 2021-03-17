ba736959fd5b031ea5a7f9e7c15667bc08c59ece
DGichuru <dangichuru77@gmail.com>
2021-03-17 23:00:39 +0300

    views added

--- todo/todo1/views.py
+++ todo/todo1/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import TodoItem
 '''def todoView(request):
     return HttpResponse('hello, this is todoView.')'''
 
-def todoView(request):
+'''def todoView(request):
     return render(request, 'todo.html')
+'''
+def todoView(request):
+    all_todo_items = TodoItem.objects.all()
+    return render(request, 'todo.html',{'all_items' :all_todo_items})
+    
